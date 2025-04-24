57d3cfcddbf0bc727f74132dd5915e9d293ee835
Harrison Held <harrydheld@gmail.com>
2025-04-24 18:25:10 -0500

    added another employee to my wonderful corporation

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 from database import Person, Company, Employment
 
 if __name__ == "__main__":
-    person = Person(name="Harrison")
-    person.save()
 
-    company = Company(name="HarrisonCorp")  # kinda like LexCorp but good instead of evil
-    company.save()
+    # make people
+    harrison = Person(name="Harrison")
+    harrison.save()
 
+    nadia = Person(name="Nadia")
+    nadia.save()
+
+    # make companies
+    harrisoncorp = Company(name="HarrisonCorp")  # kinda like LexCorp but good instead of evil
+    harrisoncorp.save()
+
+    # make jobs
     one_bajillion_dollars = 1000000000
-    employment = Employment(person_id=person.id, company_id=company.id, salary=one_bajillion_dollars)
-    employment.save()
+    harrisonjob = Employment(person_id=harrison.id, company_id=harrisoncorp.id, salary=one_bajillion_dollars)
+    harrisonjob.save()
 
-    employment.print()
+    nadia_salary = harrisonjob.salary - 1  # boss gets paid the most
+    nadiajob = Employment(person_id=nadia.id, company_id=harrisoncorp.id, salary=nadia_salary)
+    nadiajob.save()
+
+    # print
+    harrisonjob.print()
+    nadiajob.print()
